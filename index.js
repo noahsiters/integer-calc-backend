a5d01f4325c0ad6a79a5917a2690f046c11c2a0a
Noah Siters <noahsiters@Noahs-MacBook-Pro.local>
2022-07-05 12:52:03 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+var express = require('express');
+var app = express();
+var PORT = 8080;
+
+/*app.get('/calc', (req, res) => {
+    res.status(200).send({
+        message: 'ok'
+    })
+});*/
+
+app.get('/calc/:equation', function (req, res) {
+    var equation = req.params.equation;
+    var calculation = equation.match(/\d+|[^0-9]/g);
+    var result;
+
+    for (var i = 0; i < calculation.length; i++) {
+        if (i ==0) {
+            result = parseInt(calculation[i]);
+        }
+        else {
+            if (!isNaN(calculation[i])) {
+                switch (calculation[i - 1]) {
+                    case "-":
+                        result -= parseInt(calculation[i]);
+                        break;
+                    case "+":
+                        result += parseInt(calculation[i]);
+                        break;
+                    case "*":
+                        result *= parseInt(calculation[i]);
+                        break;
+                }
+            }
+        }
+    }
+    res.send(result.toString());
+})
+
+app.get('/', function (req, res) {
+    res.send('hello world!')
+})
+
+app.listen(PORT, function () {
+    console.log('currently running calculator API')
+})
